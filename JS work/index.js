3f9af2bba631e2eb2e4f9e4ece1bb7a840423d99
Lakshya-Bhatia <lakshyabhatia100@gmail.com>
2024-10-15 10:28:19 +0530

    amazon page

---
+++ JS work/index.js
@@ -0,0 +1,14 @@
+// console.log("hello Js")
+// const a=12;
+// if(a>10){
+//     let b=23;
+//     a=40;
+//     console.log("a="+a);
+// }
+// console.log("hi, a="+b)
+function fun(a,b){
+    console.log("heeee");
+    return a+b
+}
+let data=fun();
+console.log(data);
