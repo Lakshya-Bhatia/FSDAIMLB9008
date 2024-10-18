fad9a30cbf2ba72fd7d1a8f116446855a7ce923a
Lakshya-Bhatia <lakshyabhatia100@gmail.com>
2024-10-18 10:13:34 +0530

    js work and dom

--- JS work/index.js
+++ JS work/index.js
@@ -5,10 +5,58 @@
 //     a=40;
 //     console.log("a="+a);
 // }
-// console.log("hi, a="+b)
-function fun(a,b){
-    console.log("heeee");
-    return a+b
-}
-let data=fun();
-console.log(data);
+
+// function fun(a,b,c=9){
+//     console.log("heeee");
+//     return a+b+c
+// }
+// let data=fun(12,5);
+// console.log(data);
+// function ccompiler(){
+//     return "C compiler"
+// }
+// function JavaCompiler(){
+//     return "java compiler"
+// }
+// function selectlanguage(clbk){
+//     return "you have selected:"+ clbk()
+// }
+
+// const result=selectlanguage(ccompiler)
+// console.log(result)
+
+// function selectlanguage(language){
+//     let data
+//     if(language=='c'){
+//     function ccompiler(){
+//         return "C compiler"
+//     }
+//     data=ccompiler(); 
+// }
+
+//     if (language=='java'){
+
+//     function JavaCompiler(){
+//         return "java compiler"
+//     }
+//     data=JavaCompiler();
+// }
+//     return data
+// }
+
+// const result=selectlanguage('c')
+// console.log(result)
+
+console.dir(document)
+ const div=document.getElementsByClassName("parent")
+ div[0].innerHTML="<h2 style=color:red> hello Dom</h2>"
+ const h1=document.createElement("h1")
+ h1.innerText="this is dom"
+ h1.style.backgroundColor="cyan"
+ console.log(h1)
+ div[0].appendChild(h1)
+ const image=document.createElement("img")
+ image.src ='./download.jpeg'
+ image.setAttribute("height","200")
+ console.log(img)
+ div[0].appendChild(img)
