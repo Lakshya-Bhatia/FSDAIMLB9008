4a35be52863f9e11b39ef15f5f56b8806b81aba0
Lakshya-Bhatia <lakshyabhatia100@gmail.com>
2025-02-27 12:07:14 +0530

    27-feb-2025

--- backend_sec_A_day1/Product_app/server/App.js
+++ backend_sec_A_day1/Product_app/server/App.js
@@ -1,44 +1,100 @@
-const express=require("express");
+const express = require('express')
 const port = 3001;
-const app=express();
-const products=[{
-    id: 2007 ,
-    title:"Laptop",
-    price: 45000,
-    quantity:10,
+const app = express();
+const products = [{
+    id: 1001,
+    title: "LAPTOP",
+    price: 51000,
+    quantity: 5
+}, {
+    id: 1002,
+    title: "MOBILE",
+    price: 11000,
+    quantity: 2
+}
+];
+app.use(express.json());
 
-},
-{
-    id:1001,
-    title:"PHONE",
-    price: 6999,
-    quantity:6,
-}]
-app.use(express.json())
-app.get("/products",(req,res)=>{
+app.get("/products", (req, res) => {
     res.status(200);
     res.json(products);
-})
-app.post("/products",(req,res)=>{
-    const {id,title,price,quantity}=req.body;
-    const  newpro={
-        id:id,
-        title:title,
-        price:price,
-        quantity:quantity, 
+
+
+});
+
+app.get("/product/:id", (req, res) => {
+    const pid = req.params.id;
+    const index = products.findIndex(ind => ind.id == pid);
+    if (index == -1) {
+        res.status(400).json({ status: "fail", message: "product id not found" })
     }
-    products.push(newpro);
-    res.status(201).json({status:"sucess",message:"product created",newpro}) 
+    else {
+        res.status(200).json({ status: "success", message: "product id found", data: products[index] })
+    }
 })
 
- 
-app.listen(port,(err)=>{
+app.post("/product", (req, res) => {
+    const { title, price, quantity } = req.body;
+    if (title && price && quantity) {
+        const newId = products.length > 0 ? products[products.length - 1].id + 1 : 1001;
+        const newProduct = {
+            id: newId, title, price, quantity
+
+        }
+        products.push(newProduct);
+        res.status(201).json({ status: "success", message: "Product creates successfully", newProduct });
+    }
+    else {
+        res.status(400).json({ status: "failes", message: "All fields are required" })
+    }
+
+
+});
+
+
+app.patch("/editproduct/:id", (req, res) => {
+    const pid = req.params.id;
+    const { title, price, quantity } = req.body;
+    if (!title || !price || !quantity) {
+        res.status(400).json({ status: "fail", message: "All fields required except id" })
+    }
+    
+    else {
+        const index = products.findIndex(ind => ind.id == pid);
+        if (index==-1){
+            res.status(400).json({status:"fail",message:"product not found"})
+        }
+        else{
+        products[index].title = title;
+        products[index].price = price;
+        products[index].quantity = quantity;
+        res.status(200).json({ status: "success", message: "data edited sucessfully", data: products[index] })
+    }}
+
+})
+
+app.delete("/deleteproduct/:id",(re,res)=>{
+    const pid = req.params.id;
+    const index=products.findIndex(ind=>ind.id==pid)
+    if (index==-1){
+        res.status(400).json({status:"fail",message:"product not found"})
+    }
+    else{
+        const deleteddata=products.splice(index,1);
+        res.status(200).json({status:"sucsess",message:"product deleted successfully",data: deleteddata})
+    }
+
+})
+
+
+app.listen(port, (err) => {
     try {
         if (err) throw err;
-        else
-        console.log(`server is running on port ${port}`);
-        
-    } catch (err) {
-        console.log("server error",err.message);
+        else {
+            console.log(`Server is running at port ${ port }`);
+        }
+    }
+    catch (err) {
+        console.log("Server Error: ", err.message);
     }
 })
