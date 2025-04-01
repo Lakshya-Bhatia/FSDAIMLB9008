1d9ccd7a72f0be198f07dc37459e59763fcf0298
Lakshya-Bhatia <lakshyabhatia100@gmail.com>
2025-04-01 12:17:11 +0530

    01-04-2025

--- backend_sec_A_day1/Product_app/server/mongodb.js
+++ backend_sec_A_day1/Product_app/server/mongodb.js
@@ -1,13 +1,20 @@
 const {MongoClient}= require("mongodb")
 const Mongo_url="mongodb+srv://Lakshya_bhatia:Pagal123@cluster0.u0bycfw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const db_name="Lak_Database"
+const db_connect=async () => {
+    const client = new MongoClient(Mongo_url)
+    try{
 
-const client=new MongoClient(Mongo_url);
-MongoClient.connect(Mongo_url)
-.then(client=>{
-    console.log("Mongodb connected")
-})
-.catch((err) => {
-    console.log("Db",err);
-})
-
-
+        await client.connect(Mongo_url)
+        console.log("DB connected succefully")
+        const db=client.db(db_name)
+        const usercol=db.collection("Users")
+        const res = await usercol.find().toArray();
+        console.log("Data :", res)
+    }
+    catch(err){
+        console.log("error didn't connect",err.message)
+    }
+    
+}
+db_connect();
